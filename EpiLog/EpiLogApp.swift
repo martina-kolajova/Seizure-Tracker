8440c5c0c4d4975e95b5400a61367c1b3ad802a4
martina-kolajova <kolajova.ma@gmail.com>
2026-06-02 13:28:52 +0200

    chore(app): rename @main struct to EpiLogApp

---
+++ EpiLog/EpiLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpiLogApp.swift
+//  EpiLog
+//
+//  Created by Martina Kolajová on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct EpiLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
